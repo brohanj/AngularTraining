9924e0416effa770d9c95caef8c9d011a2395ad8
brohanj <31551435+brohanj@users.noreply.github.com>
2018-09-05 15:13:46 +0100

    exercise 10 - update

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -3,10 +3,7 @@ import { Contact } from 'src/app/models/contact';
 import { ContactsService } from 'src/app/contacts.service';
 import { Observable, merge } from 'rxjs';
 import { Subject } from 'rxjs/internal/Subject';
-import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
-import { FormControl } from '@angular/forms';
-// import { Observable, Subject, asapScheduler, pipe, of, from, interval, merge, fromEvent } from 'rxjs';
-
+import { debounceTime, distinctUntilChanged, switchMap, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'trm-contacts-list',
@@ -14,54 +11,33 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./contacts-list.component.css']
 })
 export class ContactsListComponent implements OnInit {
-  // public contacts: Array<Contact>;
   public contacts$: Observable<Array<Contact>>;
   public terms$ = new Subject<string>();
-  // public searchField = new FormControl(); // formcontrol is a directive
 
   constructor(private contactsService: ContactsService) { }
 
   ngOnInit() {
-    // had this line originally, then switched to subscribe, then went back to this when using async pipe
-    this.contacts$ = this.contactsService.getContacts();
 
-    // this.contactsService.getContacts()
-    // .subscribe(contacts => {
-    //     this.contacts = contacts;
-    // });
-
-    // this.terms$.pipe(
-    //   debounceTime(400),
-    //   distinctUntilChanged()
-    // ).subscribe(term => this.search(term));
-
-    this.contacts$ = merge(
-      this.terms$.pipe(
-        debounceTime(400),
-        distinctUntilChanged(),
-        switchMap(term => this.contactsService.search(term))
-      ),
-      this.contactsService.getContacts()
-    );
-
-    // this.terms$.pipe(
-    //   switchMap(term => this.contactsService.search(term) ) ,
-    //   merge(this.contacts$)
+    // this.contacts$ = merge(
+    //   this.terms$.pipe(
+    //     debounceTime(400),
+    //     distinctUntilChanged(),
+    //     switchMap(term => this.contactsService.search(term))
+    //   ),
+    //   this.contactsService.getContacts()
     // );
 
-    // this.terms$.pipe(merge(this.contacts$));
+    this.contacts$ = this.terms$.pipe(
+        debounceTime(400),
+        distinctUntilChanged(),
+        startWith(''),
+        switchMap(term => this.contactsService.search(term))
+    );
 
-    // this.terms$.pipe(switchMap(x => x), merge(this.contacts$));
-
-    // this.searchField.valueChanges.subscribe(term => this.search(term));
   }
 
   public trackByContactId(index: number, contact: Contact) {
     return contact.id;
   }
-
-  // public search(term: string) {
-  //   this.contacts$ = this.contactsService.search(term);
-  // }
 
 }
