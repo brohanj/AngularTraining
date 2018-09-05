bffc487ca3a5296b2f74f2cd670ae6a76852eec0
brohanj <31551435+brohanj@users.noreply.github.com>
2018-09-05 09:54:40 +0100

    exercise 9

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Contact } from 'src/app/models/contact';
 import { ContactsService } from 'src/app/contacts.service';
+import { Observable } from 'rxjs/Observable';
+// import { Observable, Subject, asapScheduler, pipe, of, from, interval, merge, fromEvent } from 'rxjs';
+
 
 @Component({
   selector: 'trm-contacts-list',
@@ -8,17 +11,19 @@ import { ContactsService } from 'src/app/contacts.service';
   styleUrls: ['./contacts-list.component.css']
 })
 export class ContactsListComponent implements OnInit {
-  public contacts: Array<Contact>;
+  // public contacts: Array<Contact>;
+  public contacts$: Observable<Array<Contact>>;
 
   constructor(private contactsService: ContactsService) { }
 
   ngOnInit() {
-    // this.contacts = this.contactsService.getContacts();
+    // had this line originally, then switched to subscribe, then went back to this when using async pipe
+    this.contacts$ = this.contactsService.getContacts();
 
-    this.contactsService.getContacts()
-    .subscribe(contacts => {
-        this.contacts = contacts;
-    });
+    // this.contactsService.getContacts()
+    // .subscribe(contacts => {
+    //     this.contacts = contacts;
+    // });
   }
 
   public trackByContactId(index: number, contact: Contact) {
